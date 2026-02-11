13cb1b25e435c948790ced20c3d9f01ac36f04f9
Immu <shaikimam37302@gmail.com>
2026-02-11 10:48:44 +0530

    Selection sort

---
+++ Selection_sort/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int min,temp;
+
+    for(int i=0;i<n-1;i++){
+        min=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min])
+                min=j;
+        }
+        temp = arr[i];
+        arr[i]=arr[min];
+        arr[min] = temp;   
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
